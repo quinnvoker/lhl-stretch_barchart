d637741a6f6f3277904bea236a003ad8f26b00e2
Quinn <quinnvoker@gmail.com>
2020-07-12 21:27:03 +0000

    data now spaces itself out within containing element

--- bar-chart.js
+++ bar-chart.js
@@ -3,14 +3,22 @@ $(document).ready(function() {
 });
 
 const drawBarChart= function(data, options, element) {
-  element.append(createBars(data, options));
+  element.append(createBars(data, options, element));
 }
 
-const createBars = function (data, options) {
+const createBars = function (data, options, element) {
   let bars = [];
-  for(let currentData of data) {
+  const barWidth = element.width() / data.length;
+  for(let i = 0; i < data.length; i++) {
+    const currentData = data[i];
     let bar = $('<div class="bar">' + currentData + '</div>');
-    bar.css({'display': 'inline'});
+    bar.css({
+      'position': 'absolute',
+      'bottom': 0,
+      'left': barWidth * i,
+      'width': barWidth,
+    });
+    bar.width = 200;
     bars.push(bar);
   }
   return bars;
