51403e1cc94a4451507bf40ff842071c78edf983
Quinn <quinnvoker@gmail.com>
2020-07-19 03:01:21 +0000

    updated demo page

--- demo.js
+++ demo.js
@@ -1,9 +1,9 @@
-const defaultOptions = {
+const demoOptions = {
   pixelsPerUnit: 16,
   maxValue: 20, // largest value to display within chart
   markFrequency: 5, // number of units between ticks on the y axis
-  spacing: 50,
-  title: '',
+  spacing: 20,
+  title: 'Strength of the Bending Nations',
   titleSize: 'large',
   titleColor: 'black',
   titleAlign: 'left',
@@ -15,7 +15,24 @@ const defaultOptions = {
   ruleStyle: '1px dotted black', // style for rule lines, uses 'border' format
 }
 
-const dummyData = [
+let demoOptionsMulti = {
+  pixelsPerUnit: 16,
+  maxValue: 20, // largest value to display within chart
+  markFrequency: 5, // number of units between ticks on the y axis
+  spacing: 20,
+  title: 'Strength of the Bending Nations (Allied)',
+  titleSize: 'large',
+  titleColor: 'black',
+  titleAlign: 'left',
+  barColor: 'grey', // color for any bars that don't have their colour set by data
+  barValueAlign: 'middle', // vertical alignment of value labels in bars
+  labelSize: 'medium', // font size to use for value markers and bar labels
+  labelColor: 'black', // default colour for all text in the chart
+  axisStyle: '1px solid black', // style for axis lines, uses 'border' format
+  ruleStyle: '1px dotted black', // style for rule lines, uses 'border' format
+}
+
+const demoData = [
   {
     label: 'Water',
     value: 4,
@@ -38,7 +55,7 @@ const dummyData = [
   }
 ]
 
-const dummyDataMulti = [
+const demoDataMulti = [
   [
     {
       label: 'Water',
@@ -68,5 +85,6 @@ const dummyDataMulti = [
 ]
 
 $(document).ready(function() {
-  drawBarChart(dummyDataMulti, defaultOptions, $('.bar-chart'));
+  drawBarChart(demoData, demoOptions, $('.bar-chart'));
+  drawBarChart(demoDataMulti, demoOptionsMulti, $('.bar-chart-multi'));
 });
